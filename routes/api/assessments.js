90d5d87899a00f4a3df71af1e256f766b081862a
reemalik <47259821+reemalik@users.noreply.github.com>
2019-03-02 01:10:45 +0200

    Update assessments.js

--- routes/api/assessments.js
+++ routes/api/assessments.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+const Joi = require ('joi');
 router.use(express.json())
 // We will be connecting using database 
-const Course = require('../../models/Assessments')
+const Assessment = require('../../models/Assessment')
 
 // temporary data created as if it was pulled out of the database ...
-var courses = [
-    new Assessments('1','Peter','Slim Abdelnadher','CS1', 'GUC', '+2012345', 'Monday,Tuesday'),
-    new Assessments('2','Liam','Aysha El-Safty','SE', 'GUC', '+2657764334', 'Saturday,Tuesday'),
-    new Assessments('3','Talla','Alaa Abdelateef','Physics 1', 'GUC', '+45657834', 'Sunday'),
-    new Assessments('4','Eleanor','Hany El-Sharkawy','Math 1', 'GUC', '+09876543', 'Monday,Wednesday'),
-    new Assessments('5','Elizabeth','Yasser Hegazy','Circuits 2', 'GUC', '+87654324', 'Sunday, Tuesday'),
+var assessments = [
+    new Assessment('1','Peter','Slim Abdelnadher','CS1', 'GUC', '+2012345', 'Monday,Tuesday'),
+    new Assessment('2','Liam','Aysha El-Safty','SE', 'GUC', '+2657764334', 'Saturday,Tuesday'),
+    new Assessment('3','Talla','Alaa Abdelateef','Physics 1', 'GUC', '+45657834', 'Sunday'),
+    new Assessment('4','Eleanor','Hany El-Sharkawy','Math 1', 'GUC', '+09876543', 'Monday,Wednesday'),
+    new Assessment('5','Elizabeth','Yasser Hegazy','Circuits 2', 'GUC', '+87654324', 'Sunday, Tuesday'),
 ];
 //get all assessments
 router.get('', (req, res) => {
@@ -19,57 +20,79 @@ router.get('', (req, res) => {
 })
 //create a new assessment
 router.post('/',  (req, res) => {
-
-    const name = req.body.name;
-    const expert_name = req.body.expert_name;
-    const master_class = req.body.master_class;
-    const phone_number = req.body.phone_number;
-
-
-    if(!name) return res.status(400).send({err: 'This field is required'});
-    if(!expert_name) return res.status(400).send({err: 'This field is required'});
-    if(!master_class) return res.status(400).send({err: 'This field is required'});
-    if(!phone_number) return res.status(400).send({err: 'This field is required'});
-    if (typeof name !== 'string') return res.status(400).send({err: 'Please the name using literals'});
-
-
-    
-    const newAssessment = {
-        name,
-        expert_name,
-        master_class,
-        phone_number,
-    };
-    return res.json ({data : newAssessment});
-});
-
-router.post('/joi', (req, res) => {
-    const name = req.body.name;
-    const expert_name = req.body.expert_name;
-    const master_class = req.body.master_class;
-    const phone_number = req.body.phone_number;
-
-
-    const assess = {
-        name: Joi.string().min(3).required(),
-        expert_name: Joi.string().min(3).required(),
-        master_class: Joi.string().min(3).required(),
-        phone_number: Joi.number().required(),
+    const schema = {
+    name : Joi.string().required(),
+    expertName : Joi.string().required(),
+    masterClass : Joi.string().required(),
+    daysAvail : Joi.string().required(),
+    phoneNumber : Joi.number().required()
         
     }
 
-    const result = Joi.validate (req.body, schema);
-
-    if (result.error) 
-    return res.status(400).send ({ error: result.error.details[0].message});
-
     
-     const newAssessment = {
+    const finalRes = Joi.validate (req.body, schema);
+    
+   if (finalRes.error) return res.status(400).send ({error: finalRes.error.details[0].message});
+    const name = req.body.name
+    const expertName = req.body.expertName
+    const masterClass = req.body.masterClass
+    const daysAvail = req.body.daysAvail
+    const phoneNumber = req.body.phoneNumber
+    
+    const assessment = new Assessment (
         name,
-        expert_name,
-        master_class,
-        phone_number,
-    };
-    return res.json ({data : newAssessment});
+        expertName,
+        masterClass,
+        daysAvail,
+        phoneNumber
+    )
+    assessments.push (assessment)
+    res.send(assessments)
 });
-module.exports = router;
+
+router.put('/:id', (req, res) => {
+    const schema = {
+        name : Joi.string(),
+        expertName : Joi.string(),
+        masterClass: Joi.string(),
+        daysAvail: Joi.string(),
+        phoneNumber : Joi.number()
+    }
+
+const finalRes = Joi.validate (req.body, schema);
+    if (finalRes.error) return res.status(400).send ({error: finalRes.error.details[0].message});
+   const name = req.body.name
+   const expertName = req.body.expertName
+   const masterClass = req.body.masterClass
+   const daysAvail = req.body.daysAvail
+   const phoneNumber = req.body.phoneNumber
+   const id = req.params.id;
+    
+    const assessment= assessments.find (assessment => assessment.id === id)
+    
+    if (name !== undefined)
+        assessment.name = name
+     if (expertName !== undefined)
+        assessment.expertName = expertName
+     if (masterClass !== undefined)
+        assessment.masterClass = masterClass
+     if (daysAvail !== undefined)
+        assessment.daysAvail = daysAvail
+     if (phoneNumber !== undefined)
+        assessment.phoneNumber = phoneNumber
+    
+    res.send(assessments)
+})
+
+router.delete('/id:' , (req, res) => {
+  
+    const assessmentID = req.params.id
+    const assessment = assessments.find(assessment => assessment.id === assessmentID)
+    const i = assessments.indexOf (assessment)
+    assessments.splice(i,1)
+    res.send(assessments)
+    
+})
+
+router.get ('/', (req,res) => res.json({ data: assessments}))
+module.exports = router
