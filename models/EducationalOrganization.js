fed70a3958b9194f8b700658a022e618f1d243de
SalmaMohamed7 <salma.1471998@gmail.com>
2019-03-20 22:45:20 +0200

    updated EduOrg model using mongo

--- models/EducationalOrganization.js
+++ models/EducationalOrganization.js
@@ -1,23 +1,93 @@
-const uuid = require('uuid')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-class EducationalOrganization{
-    constructor(userName, name, password, email, masterClasses, courses, workshops,
-        trainers, educators, trainingPrograms, description,contract,expirationDate) {
-        this.id = uuid.v4();
-        this.userName = userName;
-        this.name = name;
-        this.password = password;
-        this.email = email;
-        this.masterClasses = masterClasses;
-        this.courses = courses;
-        this.workshops = workshops;
-        this.trainers = trainers;
-        this.educators = educators;
-        this.trainingPrograms = trainingPrograms;
-        this.description = description;
-        this.contract = contract;
-        this.expirationDate = expirationDate;
+// Create the schema
+const EducationalOrganizationSchema = new Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String, 
+        required: true
+    },
+    masterClasses: {
+        type: [String]
+    },
+    courses:{
+        type: [String]
 
-};
-}
-module.exports = EducationalOrganization;
+    },
+    workshops:{
+        type: [String]
+
+    },
+    trainers:{
+        type: [String]
+
+    },
+    educators:{
+        type: [String]
+
+    },
+    trainingPrograms:{
+        type: [String]
+
+    },
+    description:{
+        type: String
+
+    },
+    contract:{
+        type: boolean
+
+    },
+    expirationDate:{
+        type: Date
+
+    }
+
+
+})
+
+module.exports = EducationalOrganization = mongoose.model('educationalOrganizations', EducationalOrganizationSchema)
+
+
+
+
+
+// const uuid = require('uuid')
+
+// class EducationalOrganization{
+//     constructor(userName, name, password, email, masterClasses, courses, workshops,
+//         trainers, educators, trainingPrograms, description,contract,expirationDate) {
+//         this.id = uuid.v4();
+//         this.userName = userName;
+//         this.name = name;
+//         this.password = password;
+//         this.email = email;
+//         this.masterClasses = masterClasses;
+//         this.courses = courses;
+//         this.workshops = workshops;
+//         this.trainers = trainers;
+//         this.educators = educators;
+//         this.trainingPrograms = trainingPrograms;
+//         this.description = description;
+//         this.contract = contract;
+//         this.expirationDate = expirationDate;
+
+// };
+// }
+// module.exports = EducationalOrganization;
