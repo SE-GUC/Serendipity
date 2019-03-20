170957cde39171298a326e508eb73405741f3982
MayarMahmoud <mayar.khalifa@student.guc.edu.eg>
2019-03-20 22:50:48 +0200

    created courses and workshops schemas

--- models/Workshop.js
+++ models/Workshop.js
@@ -1,16 +1,58 @@
-const uuid = require('uuid')
+// const uuid = require('uuid')
 
-class Workshop {
-    constructor(title, eduOrganisation, duration, educator, price, decription, location) {
-        this.id = uuid.v4();
-        this.title = title;
-        this.eduOrganisation = eduOrganisation;
-        this.duration = duration;
-        this.educator = educator;
-        this.price = price;
-        this.description = decription;
-        this.location = location;
-    };
-}
+// class Workshop {
+//     constructor(title, eduOrganisation, duration, educator, price, decription, location) {
+//         this.id = uuid.v4();
+//         this.title = title;
+//         this.eduOrganisation = eduOrganisation;
+//         this.duration = duration;
+//         this.educator = educator;
+//         this.price = price;
+//         this.description = decription;
+//         this.location = location;
+//     };
+// }
 
-module.exports = Workshop;
+// module.exports = Workshop;
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// Create the schema
+const BookSchema = new Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    eduOrganisation: {
+        type: string,
+        required: true
+    },
+    duration: {
+        type: Number, 
+        required: true
+    },
+    educator: {
+        type: string,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    decription: {
+        type: string,
+        required: true
+    },
+    location: {
+        type: string,
+        required: true
+    }
+
+})
+
+module.exports = Book = mongoose.model('workshop', WorkshopSchema)
