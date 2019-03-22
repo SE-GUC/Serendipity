e5c81edee1a4465923244083187c1c7eb4bb6e8f
“LamaIhab” <lamloma19@hotmail.com>
2019-03-22 16:53:29 +0200

    added update to admin using mongo

--- routes/api/admins.js
+++ routes/api/admins.js
@@ -65,6 +65,25 @@ router.post('/', async (req,res) => {
 
 
 
+ router.put('/:id', async (req,res) => {
+    try {
+     const id = req.params.id
+     const admin = await Admin.findOne({id})
+     if(!admin) return res.status(404).send({error: 'Admin does not exist'})
+     const isValidated = validator.updateValidation(req.body)
+     if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
+     const updatedAdmin = await Admin.updateOne(req.body)
+     res.json({msg: 'Admin updated successfully'})
+    }
+    catch(error) {
+        // We will be handling the error later
+        console.log(error)
+    }  
+ })
+
+
+
+
 // router.get('/', (req, res) => {
 //     let data = "";
 //     admins.forEach((value) => {
