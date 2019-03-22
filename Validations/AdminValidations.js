c12d577a9937f3de8f083e6a01cc55f54cbd1030
“LamaIhab” <lamloma19@hotmail.com>
2019-03-22 17:26:53 +0200

    updated validations for update

--- Validations/AdminValidations.js
+++ Validations/AdminValidations.js
@@ -14,10 +14,10 @@ module.exports = {
 
     updateValidation: request => {
         const updateSchema = {
-            full_name: Joi.string().required(),
-            username: Joi.string().required () ,
-            password: Joi.string().required().min(8).regex(/[!@#$%^&*()_+\-=\[\]{};':'\\|,.<>\/?]/),
-            email: Joi.string().email().required()
+            full_name: Joi.string(),
+            username: Joi.string() ,
+            password: Joi.string().min(8).regex(/[!@#$%^&*()_+\-=\[\]{};':'\\|,.<>\/?]/),
+            email: Joi.string().email()
             
         }
 
