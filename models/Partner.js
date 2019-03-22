74c9c38421f7148ee36647924340f5dadafe841d
marinanader <marina.eskander@student.guc.edu.eg>
2019-03-22 06:56:50 +0200

    updated partner's schema

--- models/Partner.js
+++ models/Partner.js
@@ -1,4 +1,22 @@
 //The Partner Model
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const PartnerSchema = new Schema({
+    email: {type: String, required: true}, //to do later: correct email syntax + unique
+    name: {type: String,required: true},
+    password: {type: String,required: true,/*minlength: 6*/},
+    description: {type: String},
+    partners: [{type: ObjectId, ref: 'Partner'}],
+    boardOfMembers: {type: [String] },
+    fieldOfWork: {type: String }, 
+    vacancies: {type: [String]},
+     pastProjects: {type: [String]}
+})
+module.exports = Partner = mongoose.model('partners', PartnerSchema)
+
+////////////////
+/*
 class Partner{
     constructor(email, username, name, password, description, partners, boardOfMembers, fieldOfWork,
         vacancies,pastProjects) {
@@ -17,3 +35,4 @@ class Partner{
 }
 
 module.exports = Partner;
+*/
