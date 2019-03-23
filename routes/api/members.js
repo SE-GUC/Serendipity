3b1a28677595b60c0ed19b5e5027db35a88fd70f
hagerhilal <hagerhilal2@gmail.com>
2019-03-23 06:10:46 +0200

    member crud using mongo

--- routes/api/members.js
+++ routes/api/members.js
@@ -32,68 +32,68 @@ router.post('/', async (req, res) => {
        } 
 
 });
+router.get("/:_id", (req, res) => {
+
+    const id = req.params._id;
+    Member.findById(id)
+      .exec()
+      .then(r => {
+        if (r) {
+          res.status(200).json(r);
+        } else {
+          res
+            .status(404)
+            .json({ message: "Member Not Found" });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: err });
+      });
+  });
 /*
-router.get( '/:username' ,(req,res)=>{
-   
-    const userName = req.params.username ;
-    const member = Member.find({userName});
-    res.json(member)
+  router.get("/:userName", (req, res) => {
+
+    const username = req.params.userName;
+    Member.find(username)
+      .exec()
+      .then(r => {
+        if (r) {
+          res.status(200).json(r);
+        } else {
+          res
+            .status(404)
+            .json({ message: "Member Not Found" });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: err });
+      });
 });
-
-
-// update info about a member
-router.put('/:username', async(req, res) => {
-    try {
-        const username = req.params.id
-        const member = await Member.findOne({id})
-        if(!member) return res.status(404).send({error: 'Member does not exist'})
-        const isValidated = validator.updateValidation(req.body)
-        if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-        const updatedMember = await Member.updateOne(req.body)
-        res.json({msg: 'Member updated successfully'})
-       }
-       catch(error) {
-           // We will be handling the error later
-           res.json("Something went wrong")
-       } 
-});
-
-/*
-
-router.delete('/:username' , (req,res)  =>{
-    const memberUserName = req.params.username ;
-
-    const member = members.find(member => member.userName == memberUserName);
-    const index = members.indexOf(member);
-    members.splice(index,1)
-    let data = "" ;
-members.forEach((member)=>{
-data += `<br> username : ${member.userName}<br> 
-email : ${member.email}<br>
-name : ${member.name} <br>
-password : ${member.password}<br>
-available Daily hours : ${member.availableDailyHours}<br>
-location : ${member.location}<br>
-Birth Date : ${member.birthDate} <br>
-Age : ${member.age}<br>
-interests : ${member.interests} <br>
-attended Events : ${member.attendedEvents}<br>
-Previous Jobs : ${member.previousJobs}<br>
-Previous Tasks : ${member.previousTasks} <br>
-Reviews  : ${member.reviews}<br>
-Reviwers : ${member.reviewers}<br>
-Certificates : ${member.certificates} <br>
-Courses Taken : ${member.coursesTaken} <br>
-Contract Signed : ${member.contractSigned}<br>
-Expiration Date : ${member.expirationDate}<br>
-______________________________________________<br>
-`;
-}); 
-res.send(data); 
-}
- 
- );
 */
+
+router.put('/:_id', async (req,res) => {
+    const isValidated = validator.updateValidation(req.body)
+    if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
+   else{
+    Member.findOneAndUpdate(req.params._id, req.body)
+    .exec()
+    .then(r => {return res.redirect(303, `/api/members/${req.params._id}`) })
+    .catch(err => {console.log(err); return res.send("No Member found") })
+   }
+});
+
+router.delete('/:_id', async (req,res) => {
+    try {
+     const deletedMember = await Member.findOneAndDelete (req.params._id)
+     res.json('Member deleted successfully')
+    }
+    catch(error) {
+        console.log(error)
+    }
+ })
+
 module.exports = router
 
 
