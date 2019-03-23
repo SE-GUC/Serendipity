1131cfd996ae1ac25eaf561d0d77fce25428af7b
yaraamer <yara.ym@gmail.com>
2019-03-23 23:44:41 +0200

    Update masterclasses.js

--- routes/api/masterclasses.js
+++ routes/api/masterclasses.js
@@ -81,7 +81,7 @@ router.get('/:id', async(req, res) => {
             });
             console.log(allCourses)
             //const educationalOrganization= await EducationalOrganization.findById(value.Eduorganizationid)
-            data+= `Id: ${value.id}<br>Name: ${value.title}<br>eduOrganisation: ${value.Eduorganization}<br>duration: ${value.duration}<br>price: ${value.price}<br>description: ${value.description}<br>location: ${value.location}<br>workshops: ${allworkshops}<br>courses: ${value.courseIDs}`;
+            data+= `Id: ${value.id}<br>Name: ${value.title}<br>eduOrganisation: ${value.Eduorganizati}<br>duration: ${value.duration}<br>price: ${value.price}<br>description: ${value.description}<br>location: ${value.location}<br>workshops: ${allworkshops}<br>courses: ${value.courseIDs}`;
             // return;
         }
     }catch(error)
