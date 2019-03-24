37a5c477c05ac567c5cb391c6d8f89fd6b196ad0
yaraan98 <yaraan98@gmail.com>
2019-03-24 23:03:22 +0200

    changed port value

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ app.use((req, res) => {
 
 
 
-const port = process.env.PORT || 3000
+var port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Server on ${port}`))
 // const port = 3000;
 // app.listen(port, () => console.log(`Server up and running on port ${port}`));
