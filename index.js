44d86b89e3da8104d00d62b02df965528fb4764b
yaraamer <yara.ym@gmail.com>
2019-03-27 18:08:45 +0200

    ..

--- index.js
+++ index.js
@@ -1,67 +1,19 @@
 
-
-
-/*const express = require("express");
-const Joi = require ('joi');
-const uuid = require ('uuid');
-const assessment = require("./routes/api/assessments");
-=======
+//mongodb+srv://YasmineMaheeb:SerendipityPassWord@cluster0-bufsj.mongodb.net/test?retryWrites=true
 const express = require('express')
 const app = express()
-//DB config
-const db = require('./config/keys').mongoURI
-const mongoose = require('mongoose')
-// Connect to mongo
-mongoose
-    .connect(db,{ useNewUrlParser: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.log(err))
-
-// Init middleware
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
 
-
-
-
-const app = express();
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send(`<h1>Welcome to LirtenHub!</h1>
-    <a href="/api/assessments">Assessments</a>`
-*/
-const express = require('express')
 const mongoose = require('mongoose')
 
+// replace the uri string with your connection string.
+// const DB_User = process.env.DB_USER;
+// const DB_Pass = process.env.DB_PASS;
+// const uri = `mongodb+srv://${DB_User}:${DB_Pass}@cluster0-bufsj.mongodb.net/test?retryWrites=true`;
 
-
-const Joi = require('joi');//not needed
-const uuid = require('uuid');//not needed
-
-const educationalOrganizations = require('./routes/api/educationalOrganizations')
-
-const courses = require('./routes/api/courses')
-const workshops = require('./routes/api/workshops')
-const members = require('./routes/api/members')
-const masterclasses = require('./routes/api/masterclasses')
-const partners = require('./routes/api/partners')
-const jobs=require('./routes/api/jobs')
-const admins = require('./routes/api/admins')
-const assessments=require('./routes/api/assessments')
-
-
-
-//yara
-//connecting to mongoDB atlas
-const app = express()
-
-
-
+// mongoose.connect("mongodb+srv://YasmineMaheeb:SerendipityPassWord@cluster0-bufsj.mongodb.net/test?retryWrites=true",{ useNewUrlParser: true })
 
 const db = require('./config/keys').mongoURI
-
-
 
 // Connect to mongo
 mongoose
@@ -73,6 +25,21 @@ mongoose
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+const Joi = require('joi');
+
+
+const educationalOrganizations = require('./routes/api/educationalOrganizations')
+const courses = require('./routes/api/courses')
+const workshops = require('./routes/api/workshops')
+const members = require('./routes/api/members')
+const masterclasses = require('./routes/api/masterclasses')
+const partners = require('./routes/api/partners')
+const jobs=require('./routes/api/jobs')
+const admins = require('./routes/api/admins')
+const assessments=require('./routes/api/assessments')
+
+
+
 
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome to LirtenHub</h1>
@@ -80,27 +47,17 @@ app.get('/', (req, res) => {
     <a href="/api/workshops">Workshops</a>
 
     <a href="/api/members">members</a>
-    <a href="/api/admins">admins</a>
+    <a href="/api/AdminProf">admins</a>A
 
+    <a href="/api/masterclasses">masterclasses</a>
 
-// Init middleware
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-
-
-app.get('/', (req, res) => {
-  res.send(`<h1>Welcome to LirtenHub</h1>
-  <a href="/api/courses">Courses</a>
-  <a href="/api/workshops">Workshops</a>
-  <a href="/api/members">members</a>
-  <a href="/api/AdminProf">admins</a>
-  <a href="/api/masterclasses">masterclasses</a>
-  <a href="/api/partners">Partners</a>
-  <a href="/api/educationalOrganizations">Educational Organizations</a>
-  <a href="/api/jobs">Job</a>
-  <a href="/api/assessments">Assessments</a>
-  `);
+    <a href="/api/partners">Partners</a>
+    <a href="/api/educationalOrganizations">Educational Organizations</a>
+    <a href="/api/jobs">Job</a>
+    <a href="/api/assessments">Assessments</a>
+    `);
 });
+
 
 // Direct routes to appropriate files
 app.use('/api/assessments', assessments)
@@ -108,10 +65,12 @@ app.use('/api/educationalOrganizations', educationalOrganizations)
 app.use('/api/courses', courses)
 app.use('/api/workshops', workshops)
 app.use('/api/members', members)
-app.use('/api/admins',admins)
+app.use('/api/AdminProf',admins)
 app.use('/api/masterclasses', masterclasses)
 app.use('/api/partners', partners)
 app.use('/api/jobs', jobs)
+
+
 
 // Handling 404
 app.use((req, res) => {
@@ -120,13 +79,11 @@ app.use((req, res) => {
 
 
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Server on ${port}`))
-// const port = 3000;
-// app.listen(port, () => console.log(`Server up and running on port ${port}`));
-
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Running server at http://localhost:${PORT}`)
+})
 /*const port = 3000
 
 app.listen(port, () => console.log(`Server up and running on port ${port}`))
 */
-
--- middleware/Logger.js
+++ middleware/Logger.js
@@ -1,4 +1,5 @@
 const moment = require('moment')
+
 const logger = (req,res,next) =>  {
     console.log(`${req.protocol} to the route ${req.originalUrl} at ${moment().format()}`)
     next()
