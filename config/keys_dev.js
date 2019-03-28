9f1716679f65baf093b7fc4e68d6adc752bbf2b8
yaraamer <yara.ym@gmail.com>
2019-03-28 22:28:11 +0200

    .

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -4,6 +4,6 @@ module.exports = {
 
 
 
-    mongoURI: 'mongodb+srv://username:<password>@cluster0-c91mn.mongodb.net/test?retryWrites=true',
+    mongoURI: 'mongodb+srv://Yara_mohamed:123456ym@yaramohamedcluster-g16gy.mongodb.net/test?retryWrites=true',
 }
 
