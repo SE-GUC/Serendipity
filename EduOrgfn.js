e40d012a9d514cf4db151869249bccc79be9ff78
SalmaMohamed7 <salma.1471998@gmail.com>
2019-03-31 01:13:20 +0200

    added get all test

--- EduOrgfn.js
+++ EduOrgfn.js
@@ -1,15 +1,31 @@
+// const axios = require('axios');
+// const functions = {
+// createEduOrg: async() =>{
+//     const NewEdu =await EducationalOrganization.create(
+//     {
+//     userName:'GQA',
+//     name:'GGQQAA',
+//     password:232323,
+//     email:'QGA@gg.com'
+//     });
+//     const allEdu=  await axios.get('http://localhost:3000/api/educationalOrganizations/')
+//     return allEdu.data
+// }
+// };
+// module.exports = functions;
 const axios = require('axios');
 const functions = {
-createEduOrg: async() =>{
-    const NewEdu =await EducationalOrganization.create(
-    {
-    userName:'GQA',
-    name:'GGQQAA',
-    password:232323,
-    email:'QGA@gg.com'
-    });
-    const allEdu=  await axios.get('http://localhost:3000/api/educationalOrganizations/')
-    return allEdu.data
-}
+getEduOrg: async () => {
+    const educationalOrganizations= axios.get('http://localhost:3000/api/educationalOrganizations/')
+    return educationalOrganizations
+},
+
+createEduOrg: async (data) => {
+    axios.post('http://localhost:3000/api/educationalOrganizations/',data).then(res => res.data).catch(e=>'error')
+},
+
+deleteEduOrg: async(id) => {
+    axios.delete('http://localhost:3000/api/educationalOrganizations/'+id).then(res => res.data).catch(e=>'error')}
+
 };
-module.exports = functions;
+module.exports=functions; 
--- test.js
+++ test.js
@@ -1,15 +1,37 @@
-const funcs = require('./EduOrgfn');
+// const funcs = require('./EduOrgfn');
 
-//test creat works
-test(`Create Edu Org`, async () => {
-    expect.assertions(4) //this depends on how many expect I am using
-    const response =  await funcs.createEduOrg()
-    const l=(response.data.length)-1
-    //console to see and check data
-    console.log(response.data[l-1])
-    //4 expect -> assertions(4)
-    expect(response.data[l].userName).toEqual('GQA')
-    expect(response.data[l].name).toEqual('GGQQAA')
-    expect(response.data[l].password).toEqual('232323')
-    expect(response.data[l].email).toEqual('QGA@gg.com')
+// //test creat works
+// test(`Create Edu Org`, async () => {
+//     expect.assertions(4) //this depends on how many expect I am using
+//     const response =  await funcs.createEduOrg()
+//     const l=(response.data.length)-1
+//     //console to see and check data
+//     console.log(response.data[l-1])
+//     //4 expect -> assertions(4)
+//     expect(response.data[l].userName).toEqual('GQA')
+//     expect(response.data[l].name).toEqual('GGQQAA')
+//     expect(response.data[l].password).toEqual('232323')
+//     expect(response.data[l].email).toEqual('QGA@gg.com')
+//   });
+
+const funcs = require('./EduOrgfn');
+jest.setTimeout(100000000);
+//const axios = require('axios');
+
+test('testing that get all works', async () => {
+  expect.assertions(6)
+  const response1 =  await funcs.getEduOrg()
+  await funcs.createEduOrg({userName:'uaa', name: 'a',  password: '12345678', email: 'a@gmail.com'})    
+  const response =  await funcs.getEduOrg()
+  const l = 1 + response1.data.data.length
+  expect(response1.data.data.length).toBe(1)
+
+  expect(response.data.data.length).toBe(l)
+  expect(response.data.data[l-1].userName).toEqual('uaa')
+  expect(response.data.data[l-1].name).toEqual('a')
+  expect(response.data.data[l-1].email).toEqual('a@gmail.com')
+  expect(response.data.data[l-1].password).toEqual('12345678')
+
   });
+
+  
