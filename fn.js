767c4b4d3345ce5b3a4d7f92f16fbe64319cee1c
marinanader <marina.eskander@student.guc.edu.eg>
2019-03-30 23:03:15 +0200

    created crud tests for Partner

--- fn.js
+++ fn.js
@@ -5,17 +5,17 @@ getPartners: async () => {
     return partners
 },
 getPartner: async (id) => {
-    const partner = axios.get('http://localhost:3000/api/partners/${id}')
+    const partner = axios.get('http://localhost:3000/api/partners/'+id)
     return partner
 },
 createPartner: async (data) => {
     axios.post('http://localhost:3000/api/partners/',data).then(res => res.data).catch(e=>'error')
 },
 updatePartner: async (id,data) => {
-    axios.put('http://localhost:3000/api/partners/${id}',data).then(res => res.data).catch(e=>'error')
+    axios.put('http://localhost:3000/api/partners/'+id,data).then(res => res.data).catch(e=>'error')
 },
 deletePartner: async (id) => {
-    axios.delete('http://localhost:3000/api/partners/${id}').then(res => res.data).catch(e=>'error')
+    axios.delete('http://localhost:3000/api/partners/'+id).then(res => res.data).catch(e=>'error')
 }
 };
 module.exports=functions;
--- test.js
+++ test.js
@@ -1,25 +1,98 @@
 const funcs = require('./fn');
 const axios = require('axios');
 
-test('testing that get all works', async () => {
-  expect.assertions(10)
-  const response1 =  await funcs.getPartners()
-  await funcs.createPartner({name: 'a', email: 'a@x.com', password: 'a1'})  
-  await funcs.createPartner({name: 'b', email: 'b@x.com', password: 'b1'})  
-  await funcs.createPartner({name: 'c', email: 'c@x.com', password: 'c1'})  
-  const response =  await funcs.getPartners()
-  const l = 3 + response1.data.data.length
-  expect(response.data.data.length).toBe(l)
-  expect(response.data.data[l-3].name).toEqual('a')
-  expect(response.data.data[l-3].email).toEqual('a@x.com')
-  expect(response.data.data[l-3].password).toEqual('a1')
-  expect(response.data.data[l-2].name).toEqual('b')
-  expect(response.data.data[l-2].email).toEqual('b@x.com')
-  expect(response.data.data[l-2].password).toEqual('b1')
-  expect(response.data.data[l-1].name).toEqual('c')
-  expect(response.data.data[l-1].email).toEqual('c@x.com')
-  expect(response.data.data[l-1].password).toEqual('c1')
-  
+  test('testing that get all works for partners', async () => {
+    expect.assertions(4)
+    const response1 =  await funcs.getPartners()
+    await funcs.createPartner({name: 'a', email: 'a@x.com', password: 'a1'})  
+    const response =  await funcs.getPartners()
+    const l =  response1.data.data.length +1
+    expect(response.data.data.length).toBe(l)
+    expect(response.data.data[l-1].name).toEqual('a')
+    expect(response.data.data[l-1].email).toEqual('a@x.com')
+    expect(response.data.data[l-1].password).toEqual('a1')
   });
+
+  test('testing that get works for partners', async () => {
+     expect.assertions(3)
+    await funcs.createPartner({name: 'a', email: 'a@x.com', password: 'a1'})  
+    const response =  await funcs.getPartners()
+    const l = response.data.data.length
+     const response1 =  await funcs.getPartner(response.data.data[l-1]._id)
+     expect(response1.data.name).toEqual('a')
+     expect(response1.data.email).toEqual('a@x.com')
+     expect(response1.data.password).toEqual('a1')
+    });
+  
+
+  test('testing that create works for partners', async () => {
+    expect.assertions(3)
+    const l = (await funcs.getPartners()).data.data.length
+    await funcs.createPartner({name: 'a', email: 'a@x.com', password: 'a1'})  
+    const response =  await funcs.getPartners()
+    expect(response.data.data[l].name).toEqual('a')
+    expect(response.data.data[l].email).toEqual('a@x.com')
+    expect(response.data.data[l].password).toEqual('a1')
+    });
+    
+  test('testing that update works for partners', async ()=>{
+    expect.assertions(3)
+    await funcs.createPartner({name: 'a', email: 'a@x.com', password: 'a1'})  
+    const response =  await funcs.getPartners()
+    const l = response.data.data.length
+    await funcs.updatePartner(response.data.data[l-1]._id,{name: 'm', email: 'm@x.com', password: 'm1'})
+    const response2 =  await funcs.getPartners()
+    const l2 =  response2.data.data.length
+    expect(response2.data.data[l2-1].name).toEqual('m')
+    expect(response2.data.data[l2-1].email).toEqual('m@x.com')
+    expect(response2.data.data[l2-1].password).toEqual('m1')
+
+  });
+
+  test('testing that delete works for partners', async ()=>{
+    expect.assertions(1)
+    await funcs.createPartner({name: 'a', email: 'a@x.com', password: 'a1'})  
+    const response =  await funcs.getPartners()
+    const l = response.data.data.length
+    await funcs.deletePartner(response.data.data[l-1]._id)
+    const response2 =  await funcs.getPartners()
+    expect(response2.data.data[l-1]).toEqual(undefined)
+  });
+
+  test('testing that create doesnt work with wrong data for partners', async()=>{
+    expect.assertions(1)
+    const l = (await funcs.getPartners()).data.data.length
+    await funcs.createPartner({name: 'a', email: '123'})  
+    const response =  await funcs.getPartners()
+    expect(response.data.data[l]).toEqual(undefined)
+  })
+
+  
+  test('testing that update doesnt work with wrong data for partners', async ()=>{
+    expect.assertions(3)
+    await funcs.createPartner({name: 'a', email: 'a@x.com', password: 'a1'})  
+    const response =  await funcs.getPartners()
+    const l = response.data.data.length
+    await funcs.updatePartner(response.data.data[l-1]._id,{name: 'm', email: '123'})
+    const response2 =  await funcs.getPartners()
+    expect(response2.data.data[l-1].name).toEqual('a')
+    expect(response2.data.data[l-1].email).toEqual('a@x.com')
+    expect(response2.data.data[l-1].password).toEqual('a1')
+
+  });  
+
+  // test('testing that get doesnt work with wrong data for partners', async()=>{
+  //   expect.assertions(1)
+  //   await funcs.createPartner({name: 'a', email: 'marmar@x.com', password: 'a1'})  
+  //   const response =  await funcs.getPartners()
+  //   const l = response.data.data.length
+  //   const id1 = response.data.data[l-1]._id
+  //   await funcs.deletePartner(response.data.data[l-1]._id)
+  //   const response2 = await funcs.getPartner(id1)
+  //   console.log(response2.data.email + "xx")
+  //   expect(response2.data).toEqual(undefined)
+  // })
+  
+
 
 
