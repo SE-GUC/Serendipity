730735d8d5902a7e048baafee04ab75303014274
Yara Amr Nassef <46930324+yaraan98@users.noreply.github.com>
2019-03-31 19:07:52 +0200

    Update Job.js
    member schema

--- models/Job.js
+++ models/Job.js
@@ -1,68 +1,5 @@
 
 
-// const mongoose = require('mongoose')
-// const ObjectId = mongoose.Schema.Types.ObjectId
-// const Schema = mongoose.Schema
-// const jobSchema = new Schema ({
-//     id: {
-//         type: ObjectId,
-        
-//     },
-//     title : {
-//         type : String ,
-//         required : true
-//     },
-//     state : {
-//         type: String ,
-//         required: true
-//     },
-//     startdate: {
-//         type: mongoose.Schema.Types.Date,
-//         required: true
-//     },
-//     enddate: {
-//         type:  mongoose.Schema.Types.Date,
-//         required: true
-//     },
-//     skills :{
-//         type : [String],
-//         required : true 
-//     },
-//     location: {
-//         type: String,
-//         required: true
-//     },
-//     salary: {
-//         type: Number ,
-//         required: true
-//     },
-//     candidates: {
-//         type: [String] ,
-        
-//     },
-//     dailyhours : {
-//      type : Number ,
-//      required : true 
-//     },
-//     partner : {
-//         type : String ,
-//         required : true 
-//     },
-//     description :{
-//         type : String ,
-//         required : true
-//     }
-
-
-// }) ;
-
-
-
-//module.exports = Job = mongoose.model('jobs', jobSchema)
-
-// const mongoose = require('mongoose')
-// const ObjectId = mongoose.Schema.Types.ObjectId
-// const Schema = mongoose.Schema
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Jobschema = new Schema({
@@ -88,7 +25,7 @@ const Jobschema = new Schema({
     },
     salary: {
         type: Number,
-     //   required: true
+        required: true
     },
   
     dailyhours: {
