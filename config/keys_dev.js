01d9889ecc3c34417cc83a5d759cea907c30140d
Yara Amr Nassef <46930324+yaraan98@users.noreply.github.com>
2019-03-31 19:20:09 +0200

    Revert "added test cases for admin"

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -4,7 +4,7 @@ module.exports = {
 
 
 
-    mongoURI: 'mongodb+srv://lamase:polyloly@cluster0-c91mn.mongodb.net/test?retryWrites=true',
+    mongoURI: 'mongodb+srv://yaraan98:Ya12051998@serendipity-iaawa.mongodb.net/test?retryWrites=true',
 
 }
 
