f7cad6797b7b78796abffb1a93e4c791e33d1b18
hager <hagerhilal2@gmail.com>
2019-04-06 04:57:47 +0200

    merge

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,18 +1,6 @@
 
-//module.exports = {
-
-<<<<<<< HEAD
-    //mongoURI: 'mongodb+srv://yaraan98:Ya12051998@serendipity-iaawa.mongodb.net/test?retryWrites=true',
 module.exports = {
-//   mongoURI: 'mongodb+srv://yaraan98:Ya12051998@dblab5-6u7ca.mongodb.net/test?retryWrites=true',
-   //  mongoURI:' mongodb+srv://yaraan98:Ya12051998@dblab5-6u7ca.mongodb.net/test?retryWrites=true',
 mongoURI : 'mongodb+srv://hager:HagerHilal1234@cluster0-x05ca.mongodb.net/test?retryWrites=true'
-=======
-
-
-    mongoURI: 'mongodb+srv://MayarZakaria:12345@cluster0-7b4sc.mongodb.net/test?retryWrites=true',
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
 
 }
 
--- routes/api/members.js
+++ routes/api/members.js
@@ -9,14 +9,12 @@ router.use(express.json())
 const Job =  require('../../models/Job')
 
 const Member = require('../../models/Member')
-<<<<<<< HEAD
-=======
+
 const Workshop = require('../../models/Workshop') //yara
 const objectId = require('mongoose').objectid //yara
 
 
 
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
 router.get('', async(req, res) => {
 
     const members = await Member.find()
@@ -42,18 +40,11 @@ function helper(_id){
   return _id
 }
 
-<<<<<<< HEAD
+
 async function calculateMatching(_id){
  try {
     commen = [] ;
-=======
 
-
-function calculateMatching(_id){
-
-  commen = [] ;
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
   Member.findById(_id)
 
   .exec()
@@ -61,17 +52,10 @@ function calculateMatching(_id){
   .then ( r => {
 
    memberskills = r.skills 
-<<<<<<< HEAD
   });
 const jobs = await Job.find(); 
 jobs
-=======
 
-  }); 
-
-Job.find()
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
 .exec()
 
 .then(r2 => {
@@ -87,30 +71,21 @@ Job.find()
 });
 
 })
-<<<<<<< HEAD
+
 }
 catch(error){
 return error 
 }
-=======
 
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
 return commen;
 
 };
 
 
-<<<<<<< HEAD
 async function filterbylocation (_id){
 try {
   filtered = [];
-=======
 
-function filterbylocation (_id){
-
-filtered = [];
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
 Member.findById(_id)
 
   .exec()
@@ -120,18 +95,12 @@ Member.findById(_id)
    memberlocation = r.location 
 
 });
-<<<<<<< HEAD
 const jobs = await calculateMatching(_id).find() ;
 jobs
-=======
 
-calculateMatching(_id).find()
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
 .exec()
 
 .then( r2 => {
-<<<<<<< HEAD
 r2.forEach((value) => {
   const joblocation = value.location;
   if (memberlocation == joblocation )
@@ -141,34 +110,14 @@ r2.forEach((value) => {
 catch (error){
   return error
 }
-=======
 
-  r2.forEach((value) => {
-
-    const joblocation = value.location;
-
-    if (memberlocation == joblocation )
-
-    filtered.push(value)
-
-})
-
-})
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
 return filtered;
 
 };
 
-<<<<<<< HEAD
 async function finalfilterbyavailability (_id){
   try {
-=======
 
-
-function finalfilterbyavailability (_id){
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
   recommendations = [];
 
   Member.findById(_id)
@@ -180,14 +129,9 @@ function finalfilterbyavailability (_id){
      memberlocation = r.location 
 
   });
-<<<<<<< HEAD
   const jobs = filterbylocation(_id).find();
   jobs
-=======
 
-  filterbylocation(_id).find()
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
   .exec()
 
   .then( r2 => {
@@ -199,16 +143,11 @@ function finalfilterbyavailability (_id){
       recommendations.push(value)
 
   })
-<<<<<<< HEAD
   })}
   catch (error) {
     return error
   }
-=======
 
-  })
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
 return recommendations;
 
 }
@@ -235,21 +174,7 @@ return c ;
 
 }
 
-<<<<<<< HEAD
-=======
 
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
 // create a new member and add it to the database
 
 router.post('/', async (req, res) => {
@@ -259,7 +184,6 @@ router.post('/', async (req, res) => {
         const isValidated = validator.createValidation(req.body)
 
         if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-<<<<<<< HEAD
           const username = req.body.userName;
           const found = await Member.findOne({"userName" : username});
           if ( found )
@@ -270,58 +194,25 @@ router.post('/', async (req, res) => {
           if ( found )
                return res.status(400).send({ error: "email is already in use" });
         else {
-=======
 
-        /*if ( req.body.userName){
-
-          const found = Member.find(member => member.userName === req.body.userName);
-
-          console.log(found);
-
-          if ( found )
-
-          var r = "username is already in use";
-
-              // return res.status(400).send({ error: "username is already in use" });
-
-        }*/
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
         const newMember = await Member.create(req.body)
 
         newMember.Age = Age(req.body.birthDate)
 
         res.json({msg:'Member was created successfully', data: newMember})
-<<<<<<< HEAD
     }}
   }
-=======
 
-       }
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
        catch(error) {
 
            // We will be handling the error later
 
            console.log(error)
-<<<<<<< HEAD
        }
-=======
 
-
-
-       } 
-
-
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
 });
 
 router.get("/:_id", (req, res) => {
-
-
-
     const id = req.params._id;
 
     Member.findById(id)
@@ -331,19 +222,12 @@ router.get("/:_id", (req, res) => {
       .then(r => {
 
         if (r) {
-<<<<<<< HEAD
           const ret = {};
           ret.data = r;
           ret.recommendations = finalfilterbyavailability(id);
           res.status(200).json(ret);
            res.status(200).json(r);
-=======
 
-          res.status(200).json(r);
-
-          res.json( { recommendations : finalfilterbyavailability } )
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
         } else {
 
           res
@@ -357,39 +241,20 @@ router.get("/:_id", (req, res) => {
       })
 
       .catch(err => {
-<<<<<<< HEAD
         return "Member Not Found"
      
-=======
 
-        console.log(err);
-
-        res.status(500).json({ error: err });
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
       });
 
   });
 
-<<<<<<< HEAD
 router.put('/:_id', async (req,res) => {
   try{
-=======
 
-
-
-
-
-
-
-
-router.put('/:_id', async (req,res) => {
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
     const isValidated = validator.updateValidation(req.body)
 
     if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-<<<<<<< HEAD
+
     const username = req.body.userName;
           const found = await Member.findOne({"userName" : username});
           if ( found )
@@ -412,31 +277,7 @@ router.put('/:_id', async (req,res) => {
      res.send(err)
    }
   
-=======
 
-   else{
-
-    /*if ( req.body.userName){
-
-      const found = Member.find(member => member.userName === req.body.userName);
-
-      if ( found )
-
-          return res.status(400).send({ error: "username is already in use" });
-
-  }*/
-
-    Member.findByIdAndUpdate(req.params._id, req.body)
-
-    .exec()
-
-    .then(r => {return res.redirect(303, `/api/members/${req.params._id}`) })
-
-    .catch(err => {console.log(err); return res.send("No Member found") })
-
-   }
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
 });
 
 
@@ -446,13 +287,8 @@ router.delete('/:_id', async (req,res) => {
     try {
 
      const deletedMember = await Member.findOneAndDelete (req.params._id)
-<<<<<<< HEAD
      res.json({msg :'Member deleted successfully' , data : deletedMember})
-=======
 
-     res.json('Member deleted successfully')
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
     }
 
     catch(error) {
@@ -460,19 +296,7 @@ router.delete('/:_id', async (req,res) => {
         console.log(error)
 
     }
-<<<<<<< HEAD
 })
 
 module.exports = router
-=======
 
- })
-
-
- 
-
-
-
-module.exports = router
-
->>>>>>> e18ca44cff4477d8eb4ee14ecb54b70dd805f33a
