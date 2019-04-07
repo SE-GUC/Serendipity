3d618781008ae7e1994c64bd4f47d4b9d62738fc
hager <hagerhilal2@gmail.com>
2019-04-07 06:45:44 +0200

    Member can view his/her profile (Form)

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import MemberProfile from './components/MemberProfile';
+
+
+class App extends Component {
+ render() {
+    return (
+      <div className="Member">
+     <MemberProfile/>
+      </div>
+    );
+  }
+}
+
+export default App;
--- config/keys_dev.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-mongoURI : 'mongodb+srv://hager:HagerHilal1234@cluster0-x05ca.mongodb.net/test?retryWrites=true'
-
-}
-
-
-
