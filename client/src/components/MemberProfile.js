92588ad9257293e07e967375c41863896ea82b77
hager <hagerhilal2@gmail.com>
2019-04-07 06:47:20 +0200

    member profile form

--- client/src/components/MemberProfile.js
+++ client/src/components/MemberProfile.js
@@ -56,75 +56,75 @@ getMember = (e) => {
 <input type="email" className="memberform" placeholder={this.state.email} value = {this.state.email} disabled>
 </input><br></br>
 </div>
-<div class="field">
+<div className = "field" >
 <label for="username">User name</label>
 <input type="text" className="memberform" placeholder={this.state.userName} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="name">Name</label>
 <input type="text" className="memberform" placeholder={this.state.name} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="passworf">password</label>
 <input type="password" className="memberform" placeholder= {this.state.password} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="availableDailyHours ">availableDailyHours </label><br></br>
 <input type="text" className="memberform" placeholder={this.state.availableDailyHours} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="location ">location </label>
 <input type="text" className="memberform" placeholder={this.state.location} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="birthDate ">birthDate </label>
 <input type="date" className="memberform" placeholder={this.state.birthDate} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="interests  ">interests  </label>
 <input type="array" className="memberform" placeholder={this.state.interests} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="attendedEvents  ">attendedEvents  </label>
 <input type="array" className="memberform" placeholder={this.state.attendedEvents} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="previousProjects  ">previousProjects  </label>
     <input type="array" className="memberform" placeholder={this.state.previousProjects} disabled>
 </input><br></br></div>
-<div class="field">
+<div className="field">
 <label for="previousTasks  ">previousTasks </label>
 <input type="array" className="memberform" placeholder={this.state.previousTasks} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="previousJobs  ">previousJobs </label>
 <input type="array" className="memberform" placeholder={this.state.previousJobs} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="review  ">review  </label>
 <input type="text" className="memberform" placeholder={this.state.reviews} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="reviewers   ">reviewers   </label>
 <input type="text" className="memberform" placeholder={this.state.reviewers} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="certificates   ">certificates  </label>
 <input type="text" className="memberform" placeholder={this.state.certificates} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="coursesTaken   ">coursesTaken   </label>
 <input type="text" className="memberform" placeholder={this.state.coursesTaken} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="contractSigned">contractSigned   </label>
 <input type="boolean" className="memberform" placeholder={this.state.contractSigned} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="age ">age    </label>
 <input type="number" className="memberform" placeholder={this.state.age} disabled>
 </input><br></br></div>
-<div class="field">
+<div className = "field">
 <label for="skills ">skills </label>
 <input type="array" className="memberform" placeholder={this.state.skills} disabled>
 </input><br></br></div>
