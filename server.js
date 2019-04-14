d86d61cd37632e75b06149783298824d61d7d6a3
yaraan98 <yaraan98@gmail.com>
2019-04-14 19:29:57 +0200

    make it better

--- server.js
+++ server.js
@@ -42,7 +42,15 @@ app.use(express.urlencoded({extended: false}))
 app.use(cors())
 
 
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static('client/build'));
 
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(_dirname,'client', 'build','index.html'));
+  });
+
+  
+}
 
 app.get('/', (req, res) => {
 
@@ -105,10 +113,3 @@ const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server on ${port}`))
 
-if(process.env.NODE_ENV==='production'){
-  app.use(express.static('client/build'));
-
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(_dirname,'client', 'build','index.html'));
-  });
-}
