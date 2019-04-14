fa312d9708c060764265986dd3542286bbc23384
Yara Amr Nassef <46930324+yaraan98@users.noreply.github.com>
2019-04-14 10:53:03 +0200

    Update members.js

--- server/routes/api/members.js
+++ server/routes/api/members.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-const validator = require('../../validations/memberValidations')
+const validator = require('../../Validations/memberValidations')
 
 router.use(express.json())
 
