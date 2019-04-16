6bf27fc4a91f76df671910c93aa97a95c1bb5d57
yaraan98 <yaraan98@gmail.com>
2019-04-16 02:31:14 +0200

    heroku hopefully223

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,10 +1,10 @@
 module.exports = {
 
 
-    // mongoURI: process.env.MONGO_URI,
-    // secretOrKey: process.env.SECRET_OR_KEY //trial yara herku
+    mongoURI: process.env.MONGO_URI,
+    secretOrKey: process.env.SECRET_OR_KEY //trial yara herku
     ////tr
-    mongoURI: 'mongodb+srv://mayar:mayar123456@serendipity-iaawa.mongodb.net/test?retryWrites=true',
-    secretOrKey: 'verysecretkey' 
+    // mongoURI: 'mongodb+srv://mayar:mayar123456@serendipity-iaawa.mongodb.net/test?retryWrites=true',
+    // secretOrKey: 'verysecretkey' 
     //trial
 }
