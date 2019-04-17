be42f940890d5907418ccdeafddfa6716926b840
yaraamer <yara.ym@gmail.com>
2019-04-17 22:14:21 +0200

    pulled master

---
+++ server/config/keys_dev.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+
+
+
+   // mongoURI: 'mongodb+srv://lamase:polyloly@cluster0-c91mn.mongodb.net/test?retryWrites=true'
+    mongoURI:'mongodb+srv://Yara_mohamed:123456ym@yaramohamedcluster-g16gy.mongodb.net/test?retryWrites=true',
+
+   // mongoURI: 'mongodb+srv://MayarZakaria:12345@cluster0-7b4sc.mongodb.net/test?retryWrites=true',
+
+   
+
+
+}
+
+
